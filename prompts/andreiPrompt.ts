6c00eb735fb9c9760c04b1e6b9c9a9dbe03a145e
Andrei Kozyakov <andreikozyakov@geotab.com>
2025-06-23 11:06:08 -0400

    Refactor andreiPrompt to improve error tracing instructions and formatting

--- prompts/andreiPrompt.ts
+++ prompts/andreiPrompt.ts
@@ -1,12 +1,14 @@
 export const andreiPrompt = `
 I have next exception:
 Cause: {errorMessage}
-Stack trace: {stackTrace}
+Stack trace:
+{stackTrace}
 
 I downloaded a minified JS file contains the error. I found the place using 'Line number' and 'Column number' and cut the file content to have 500 symbols before and 500 symbols after the 'Line number' and 'Column number'.
 Here is the result:
 {code}
 
+Step 1:
 Could you please help trace an error from our minified JavaScript back to the original source code?
 Task:
 The goal is to find the corresponding location in our project's source files.
@@ -20,6 +22,32 @@ Rules:
 1. Focus on TypeScript files first, as they are more likely to contain the original source
 2. The file should contain almost all the identifiers found in the snippet
 
-Result:
-Please provide at least three potential source locations (file name and line number) that are the most.
+Please provide at least three potential source locations (file name and line number) that are the most. Then go through the 3 files and find the best file where the file content mostly identical to the provided snippet of the minified code. Let's use the most promising candidate file and try to find the line where the error I prodived can happend.
+
+Mandatory:
+1)DO NOT TRY TO FIX THE ERROR WHILE YOU GET SUCH APPROVAL!
+2) DO NOT SHOW THINKING TEXT. JUST SHOW ME THE RESULT. USE EXAMPLE BELOW:
+
+Result example:
+The most likely place for this error to originate in distributionListEditor.ts is within the addRecipientHandler function:
+File: distributionListEditor.ts
+Line: Around 1435 (inside addRecipientHandler function)
+\`\`\`JavaScript
+addRecipientHandler = function (recipientType: string, recipient, lockTemplatesObj: boolean) {
+    var params = recipientKinds[recipientType]; // <--- If recipientKinds[recipientType] is undefined, 'params' will be undefined.
+
+    inlineSaveMessage && inlineSaveMessage.hideMessages();
+
+    if (params.length === 0) { // <--- This would throw "Cannot read properties of undefined (reading 'length')" if params is undefined.
+        addInternalRecipient({ recipientType }).then(function () {
+            eventsHandler.fireEvent("onChange", this, []);
+            renderRecipients();
+        });
+    } else {
+        addRecipientForm && addRecipientForm.cancel();
+        morePopupWindow.close();
+        addRecipientForm = addForm(container as HTMLElement, recipientType, recipient, addRecipient, lockTemplatesObj);
+    }
+},
+\`\`\`
 `;
