c43333a7da65426b610c060b476ee491a9efc294
Alexander Dashukevich <alexanderdashukevich@geotab.com>
2025-06-23 13:23:35 +0200

    Commented debug code.

--- src/parser.ts
+++ src/parser.ts
@@ -40,7 +40,7 @@ const parseException = (traceStr: string): IExceptionData => {
 }
 
 
-console.log(parseException(`Cause: EX8B75FD73: Cannot read properties of undefined (reading 'companyGroups')
+/*console.log(parseException(`Cause: EX8B75FD73: Cannot read properties of undefined (reading 'companyGroups')
 
 https://my296.transfloeld.com/universaltruckloadservices/dist/4325.js
 
@@ -52,3 +52,4 @@ Stack trace:
 
 //======StackTrace=======
 [{"url":"https://my296.transfloeld.com/universaltruckloadservices/dist/4325.js","lineNumber":1,"columnNumber":3280,"module":"https://my296.transfloeld.com/universaltruckloadservices/dist/4325.js","at":"Object.getCompanyGroupsNamesLine"},{"url":"https://my296.transfloeld.com/universaltruckloadservices/dist/2032.js","lineNumber":1,"columnNumber":56707,"module":"https://my296.transfloeld.com/universaltruckloadservices/dist/2032.js","at":"q"},{"url":"https://my296.transfloeld.com/universaltruckloadservices/dist/2032.js","lineNumber":1,"columnNumber":56793,"module":"https://my296.transfloeld.com/universaltruckloadservices/dist/2032.js","at":"H"},{"url":"https://my296.transfloeld.com/universaltruckloadservices/dist/2032.js","lineNumber":1,"columnNumber":56839,"module":"https://my296.transfloeld.com/universaltruckloadservices/dist/2032.js","at":"o"},{"url":"https://my296.transfloeld.com/universaltruckloadservices/dist/5351.js","lineNumber":1,"columnNumber":11451,"module":"https://my296.transfloeld.com/universaltruckloadservices/dist/5351.js","at":"Object.provideAddressOnDemand"},{"url":"https://my296.transfloeld.com/universaltruckloadservices/dist/2032.js","lineNumber":1,"columnNumber":56978,"module":"https://my296.transfloeld.com/universaltruckloadservices/dist/2032.js","at":""},{"url":"https://my296.transfloeld.com/universaltruckloadservices/dist/2032.js","lineNumber":1,"columnNumber":57056,"module":"https://my296.transfloeld.com/universaltruckloadservices/dist/2032.js","at":""},{"url":"https://my296.transfloeld.com/universaltruckloadservices/dist/2032.js","lineNumber":1,"columnNumber":56848,"module":"https://my296.transfloeld.com/universaltruckloadservices/dist/2032.js","at":"Q"},{"url":"https://my296.transfloeld.com/universaltruckloadservices/dist/2032.js","lineNumber":1,"columnNumber":60290,"module":"https://my296.transfloeld.com/universaltruckloadservices/dist/2032.js","at":"Object.drawLiveVehicle"}]`));
+*/
